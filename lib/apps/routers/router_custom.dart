760a7fedc16f9cec1ccedb69778c18ec93606d09
VanDuy <vanduy@gmail.com>
2025-09-20 20:50:39 +0700

    binding

--- lib/apps/routers/router_custom.dart
+++ lib/apps/routers/router_custom.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:music_sleeping/apps/routers/router_name.dart';
+import 'package:music_sleeping/modules/home/bindings/home_binding.dart';
 import 'package:music_sleeping/modules/home/pages/home_page.dart';
 
 class RouterCustom {
   static final initial = RouterName.home;
-  static final pages = [GetPage(name: RouterName.home, page: () => HomePage())];
+  static final pages = [
+    GetPage(
+      name: RouterName.home,
+      page: () => HomePage(),
+      binding: HomeBinding(),
+    ),
+  ];
 }
